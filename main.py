c88c58af1c72ef90451c6da577297ee8a9c6c69e
Joshua Jones <105621089+jsquaresec@users.noreply.github.com>
2026-09-12 15:13:19 -0500

    Activate threaded intelligence and network utilities

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from bot.free_person_commands import register_free_person_commands
 from bot.personpages_person_commands import register_personpages_person_commands
 from bot.provider_commands import register_provider_commands
 from bot.security_commands import register_security_commands
+from bot.threaded_intel_commands import register_threaded_intel_commands
 from bot.you_person_commands import register_you_person_commands
 from core.config import settings
 from storage.db import init_db
@@ -26,6 +27,7 @@ class FreeStackPurpleTeamBot(PurpleTeamBot):
         register_free_person_commands(self)
         register_personpages_person_commands(self)
         register_you_person_commands(self)
+        register_threaded_intel_commands(self)
 
         if settings.discord_guild_id:
             guild = discord.Object(id=settings.discord_guild_id)
